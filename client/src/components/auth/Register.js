54b28999acebf5c0826c871e6942a44340ac3b48
Jonne Liukkonen <jonne.liukkonen@gmail.com>
2019-09-15 16:49:29 +0300

    Commit before Axios

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
@@ -21,6 +22,27 @@ const useStyles = makeStyles(theme => ({
 const Register = () => {
   const classes = useStyles();
 
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Salasanat eivät täsmää');
+    } else {
+      console.log(formData);
+    }
+  };
+
   return (
     <Grid
       container
@@ -30,43 +52,65 @@ const Register = () => {
     >
       <Box>
         <Typography variant='h4'>Rekisteröidy!</Typography>
-        <TextField
-          id='name'
-          label='Nimi'
-          placeholder='Esi Merkki'
-          fullWidth
-          margin='normal'
-        />
-        <TextField
-          id='email'
-          label='Sähköposti'
-          placeholder='esi.merkki@gmail.com'
-          fullWidth
-          margin='normal'
-        />
-        <TextField
-          id='password'
-          label='Salasana'
-          placeholder='Minimissään KUUSI merkkiä pitkä'
-          fullWidth
-          type='password'
-          autoComplete='current-password'
-          margin='normal'
-        />
-        <br />
-        <TextField
-          id='password2'
-          label='Salasana uudestaan'
-          fullWidth
-          type='password'
-          autoComplete='current-password'
-          margin='normal'
-        />
-        <br />
-        <br />
-        <Button className={classes.button} color='inherit'>
-          Rekisteröidy
-        </Button>
+        <form onSubmit={e => onSubmit(e)}>
+          <TextField
+            name='name'
+            value={name}
+            onChange={e => onChange(e)}
+            label='Nimi'
+            placeholder='Esi Merkki'
+            fullWidth
+            margin='normal'
+            required
+          />
+          <TextField
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+            label='Sähköposti'
+            placeholder='esi.merkki@gmail.com'
+            fullWidth
+            margin='normal'
+            required
+          />
+          <TextField
+            name='password'
+            value={password}
+            onChange={e => onChange(e)}
+            label='Salasana'
+            placeholder='Vähintään KUUSI merkkiä pitkä'
+            fullWidth
+            type='password'
+            autoComplete='current-password'
+            margin='normal'
+            required
+          />
+          <br />
+          <TextField
+            name='password2'
+            value={password2}
+            onChange={e => onChange(e)}
+            label='Salasana uudestaan'
+            fullWidth
+            type='password'
+            autoComplete='current-password'
+            margin='normal'
+            required
+          />
+          <br />
+          <br />
+          <Button type='submit' className={classes.button} color='inherit'>
+            Rekisteröidy
+          </Button>
+        </form>
+
+        <Typography variant='subtitle2'>
+          Onko sinulla jo tili?{' '}
+          <Link className={classes.link} to='/login'>
+            Kirjaudu sisään
+          </Link>
+          .
+        </Typography>
       </Box>
     </Grid>
   );
